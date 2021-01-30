2afc91071ea27cb5569c0bf876de8cc3354dfa40
Loner1024 <xth12138@gmail.com>
2021-01-31 02:26:38 +0800

    go-web-template

--- settings/settings.go
+++ settings/settings.go
@@ -1,1 +1,23 @@
-package setting
+package settings
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func Init()(err error){
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./")
+	err=viper.ReadInConfig()
+	if err!=nil{
+		//fmt.Printf("viper.ReadInConfig() failed: %v \n",err)
+		return err
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Println("config changed")
+	})
+	return nil
+}
